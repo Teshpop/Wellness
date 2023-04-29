199446e7641026c2cba277f096b2c4d3b9e56be7
Pedro Tello <Tello.3810@outlook.com>
2023-04-28 19:36:04 -0600

    Correccion seccion programas vista celular, heades responsive y animade ocultar header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Logo from './assets/img/WellnessLogo.png'
 import portada from './assets/img/Portada.png'
 import menuRs from './assets/img/MenuRS.png'
@@ -42,7 +42,7 @@ function Section2(){
   function Cont(){
     return(
       <>
-        <div className='bg-white w-[35%] flex flex-col rounded-xl py-5 pb-10 px-10 my-52 shadow-md '>
+        <div className='bg-white w-[90%] lg:w-[35%] flex flex-col rounded-xl py-5 pb-10 px-10 lg:my-52 shadow-md '>
           <div className='flex flex-row justify-center items-center py-6'>
             <h2 className=' text-3xl mx-5 '>Titulo</h2>
             <img src={prueba} className='w-16 h-16 rounded-full mx-5'/>
@@ -67,11 +67,15 @@ function Section2(){
   }
 
   return(
-    <section id='#programs' className='z-10'>
-      <div className=' w-full h-screen bg-brown-200'>
+    <section id='programs' className='z-10'>
+      <div className=' w-full h-auto bg-brown-200'>
         <div className=' flex flex-col justify-center items-center '>
           <h1 className='uppercase text-4xl md:text-5xl lg:text-6xl relative top-24'>programas</h1>
-          <div className='flex flex-row'>
+          <div className='flex flex-col lg:flex-row lg:gap-20 justify-center gap-10 items-center relative pt-40 pb-20 lg:top-0'>
+            <Cont/>
+            <Cont/>
+            <Cont/>
+            <Cont/>
             <Cont/>
             <Cont/>
             <Cont/>
@@ -110,44 +114,88 @@ function BtnRs(){
     <>
       <a href="https://www.tiktok.com/" target='_blank'>
           <img src={tiktok} alt='Logo Tik-Tok' 
-          className={`h-20 w-20 opacity-40 hover:opacity-100 fixed right-7 transition-all duration-300 ${showButtons ? `visible bottom-[24.5rem]` : 'invisible bottom-7' }`}/>
+          className={`md:h-20 md:w-20 w-[4rem] h-[4rem] opacity-40 hover:opacity-100 fixed md:right-7 right-8 transition-all duration-300 ${showButtons ? `visible md:bottom-[24.5rem] bottom-[21.7rem]` : 'invisible bottom-7' }`}/>
       </a>
       <a href="https://www.facebook.com/" target='_blank'>
           <img src={facebook} alt='Logo Facebook' 
-          className={`h-20 w-20 opacity-40 hover:opacity-100 fixed right-7 transition-all duration-300 ${showButtons ? `visible bottom-[19rem]` : 'invisible bottom-7' }`}/>
+          className={`md:h-20 md:w-20 w-[4rem] h-[4rem] opacity-40 hover:opacity-100 fixed md:right-7 right-8 transition-all duration-300 ${showButtons ? `visible md:bottom-[19rem] bottom-[16.7rem]` : 'invisible bottom-7' }`}/>
       </a>
       <a href="https://www.twitter.com/" target='_blank'>
           <img src={twitter} alt='Logo Twitter' 
-          className={`h-20 w-20 opacity-40 hover:opacity-100 fixed right-7 transition-all duration-300 ${showButtons ? `visible bottom-[13.5rem]` : 'invisible bottom-7' }`}/>
+          className={`md:h-20 md:w-20 w-[4rem] h-[4rem] opacity-40 hover:opacity-100 fixed md:right-7 right-8 transition-all duration-300 ${showButtons ? `visible md:bottom-[13.5rem] bottom-[11.7rem]` : 'invisible bottom-7' }`}/>
       </a>
       <a href="https://www.instagram.com/" target='_blank'>
           <img src={instagram} alt='Logo Instagram' 
-          className={`h-20 w-20 opacity-40 hover:opacity-100 fixed right-7 transition-all duration-300 ${showButtons ? `visible bottom-[8rem]` : 'invisible bottom-7' }`}/>
+          className={`md:h-20 md:w-20 w-[4rem] h-[4rem] opacity-40 hover:opacity-100 fixed md:right-7 right-8 transition-all duration-300 ${showButtons ? `visible md:bottom-[8rem] bottom-[7rem]` : 'invisible bottom-7' }`}/>
       </a>
       <img src={menuRs} onClick={handleClick} alt="Menu Redes Sociales" 
-      className={`w-24 h-24 fixed bottom-5 right-5 hover:opacity-100 cursor-pointer transition-all duration-300 ${showButtons ? 'opacity-100':'opacity-40'}`}/>
+      className={`md:w-24 md:h-24 w-[5rem] h-[5rem] fixed bottom-5 right-5 hover:opacity-100 cursor-pointer transition-all duration-300 sm: ${showButtons ? 'opacity-100':'opacity-40'}`}/>
     </>
   )
 }
 
+function useScrollDirection() {
+  const [scrollDirection, setScrollDirection] = useState(null);
 
+  useEffect(() => {
+    let lastScrollY = window.pageYOffset;
 
+    const updateScrollDirection = () => {
+      const scrollY = window.pageYOffset;
+      const direction = scrollY > lastScrollY ? "down" : "up";
+      if (direction !== scrollDirection && (scrollY - lastScrollY > 5 || scrollY - lastScrollY < -5)) {
+        setScrollDirection(direction);
+      }
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+    };
+    window.addEventListener("scroll", updateScrollDirection);
+    return () => {
+      window.removeEventListener("scroll", updateScrollDirection);
+    }
+  }, [scrollDirection]);
 
-function App() {
-  return (
+  return scrollDirection;
+};
+
+function Header(){
+  const [showMenu, setShowMenu] = useState(false)
+  const scrollDirection = useScrollDirection();
+  return(
     <>
-      <body className=" font-bellota mr-0 text-brown-300 font-semibold ">
-        <header className='z-10 flex flex-row justify-between bg-brown-200 h-24 w-screen bg-opacity-50 fixed'>
+      <header className={`z-10 flex flex-row justify-between bg-brown-200 h-24 w-screen bg-opacity-50 fixed drop-shadow-md ${scrollDirection === 'down' ? '-top-24': 'top-0'} transition-all duration-500`}>
           <img src={Logo} alt="Logo Welness" className=' w-16 h-16 ml-[4.563rem] mt-5 relative'/>
-          <nav className=' flex flex-row items-center justify-center w-full '>
+          <nav className=' md:flex lg:flex-row md:items-center md:justify-center md:w-full hidden'>
             <div className=' flex flex-row justify-evenly w-[95%] text-base lg:text-xl '>
               <a href="#">Nuestro método</a>
-              <a href="#">Programas</a>
+              <a href="#programs">Programas</a>
               <a href="#">Sobre Nosotros + Equipo</a>
               <a href="#">Contacto</a>
             </div>
           </nav>
+          <button className='block md:hidden py-auto px-7  rounded focus:outline-none md:hover:bg-brown-100 focus:bg-brown-100  transition-all duration-300' onClick={()=>{setShowMenu(!showMenu)}}>
+            <div className=' w-10 h-1 bg-brown-200 mb-2 '></div>
+            <div className=' w-10 h-1 bg-brown-200 mb-2 '></div>
+            <div className=' w-10 h-1 bg-brown-200 mb-2 '></div>
+            <div className={`absolute top-0 right-0 w-full h-screen transition-all duration-500 bg-brown-200 ${showMenu ? 'opacity-100' : 'opacity-0 translate-y-[-60rem]'}`}>
+              <div className='fixed flex flex-col justify-center items-center h-screen w-full text-4xl gap-36'>
+                <a href="#" className='px-10 hover:border-b-2'>Nuestro método</a>
+                <a href="#programs" className='px-10 hover:border-b-2'>Programas</a>
+                <a href="#" className='px-10 hover:border-b-2'>Sobre Nosotros + Equipo</a>
+                <a href="#" className='px-10 hover:border-b-2'>Contacto</a>
+                <a href="#" className='px-5 hover:border-b-2 text-2xl'>Cerrar</a>
+              </div>
+            </div>
+          </button>
         </header>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <div className=" font-bellota mr-0 text-brown-300 font-semibold">
+        <Header/>
         <Section0/>
         <Section1/>
         <Section2/>
@@ -155,7 +203,7 @@ function App() {
         <Section4/>
         <BtnRs/>
         <footer></footer>
-      </body>
+      </div>
     </>
   )
 }
