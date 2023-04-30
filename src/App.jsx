22b276e7b0e3c5b2c28fdc476367ba8c01bc2109
Pedro Tello <Tello.3810@outlook.com>
2023-04-30 15:10:32 -0600

    Diseño responsive programas terminado, estructura principal form

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import tiktok from './assets/img/tiktokLogo.png'
 import facebook from './assets/img/facebookLogo.png'
 import instagram from './assets/img/instagramLogo.png'
 import twitter from './assets/img/twitterLogo.png'
+import whatsapp from './assets/img/whatsappLogo.png'
 
 function Section0(){
   return(
@@ -42,7 +43,7 @@ function Section2(){
   function Cont({id, color, text}){
     return(
       <>
-        <article id={id} className={`flex-none rounded-2xl drop-shadow-md snap-center lg:w-[40%] ${color}`}>
+        <article id={id} className={`flex-none rounded-2xl drop-shadow-md snap-center w-[90%] lg:w-[40%] ${color}`}>
           <div className='flex flex-col w-full h-full items-center gap-8 py-9'>
             <div className='flex flex-row justify-center items-center gap-7'>
               <h2 className='uppercase text-lg'>Titulo</h2>
@@ -73,9 +74,9 @@ function Section2(){
 
   return(
     <section id='programs'>
-      <div className='flex flex-col bg-brown-200 justify-center w-full items-center lg:h-screen gap-5'>
+      <div className='flex flex-col bg-brown-200 justify-center w-full items-center h-screen gap-5'>
         <h1 className='uppercase text-3xl md:text-4xl lg:text-5xl'>programas</h1>
-        <div className="flex flex-row overflow-x-scroll gap-[10rem] snap-x w-full h-[70%] py-10">
+        <div className="flex flex-row overflow-hidden hover:overflow-scroll gap-5 lg:gap-[10rem] px-4 lg: snap-x w-full h-[70%] py-10">
           <Cont id={'1'} color={'bg-white'}
           text={
             'lorem ipus'
@@ -126,7 +127,34 @@ function Section3(){
 function Section4(){
   return(
     <section>
-      <h1>Formulario</h1>
+      <div className='h-screen w-full bg-brown-200 flex justify-center items-center'>
+        <div className='flex flex-row w-[85%] h-[70%] bg-brown-100 justify-center items-center gap-36'>
+          <form action="" method="post" acceptCharset='UTF-8'>
+            <fieldset className='flex flex-col gap-12'>
+              <div className='flex flex-col'>
+                <label htmlFor="name">Nombre Completo</label>
+                <input type="text" id='name' name='name' />
+              </div>
+
+              <div className='flex flex-col'>
+                <label htmlFor="email">Correo</label>
+                <input type="email" name="email" id="email" />
+              </div>
+
+              <div className='flex flex-col'>
+                <label htmlFor="numbre">Telefono</label>
+                <input type="tel" name="number" id="number" />
+              </div>
+
+              <div className='flex flex-row gap-10'>
+                <button type="submit">Enviar</button>
+                <img src={whatsapp} alt="" className='h-12 w-12 drop-shadow-md'/>
+              </div>
+            </fieldset>
+          </form>
+          <img src={Logo} alt="LogoWelness" className=' drop-shadow-2xl ' />
+        </div>
+      </div>
     </section>
   )
 }
@@ -188,16 +216,31 @@ function useScrollDirection() {
 function Header(){
   const [showMenu, setShowMenu] = useState(false)
   const scrollDirection = useScrollDirection();
+
+  function BtnMen({title,id}){
+    const [hover ,setHover] = useState(false)
+    return(
+      <>
+      <a href={id} 
+      onMouseEnter={()=>{setHover(true)}}
+      onMouseLeave={()=>{setHover(false)}}>
+        {title}
+        <div className={`h-0.5 bg-brown-300 transition-all duration-300 ${hover ? 'w-full' : 'w-0'}`}></div>
+      </a>
+      </>
+    )
+  }
+
   return(
     <>
       <header className={`z-10 flex flex-row justify-between bg-brown-200 h-24 w-screen bg-opacity-50 fixed drop-shadow-md ${scrollDirection === 'down' ? '-top-24': 'top-0'} transition-all duration-500`}>
           <a href="#home"><img src={Logo} alt="Logo Welness" className=' w-16 h-16 ml-[4.563rem] mt-5 relative'/></a>
           <nav className=' md:flex lg:flex-row md:items-center md:justify-center md:w-full hidden'>
             <div className=' flex flex-row justify-evenly w-[95%] text-base lg:text-xl '>
-              <a href="#">Nuestro método</a>
-              <a href="#programs">Programas</a>
-              <a href="#">Sobre Nosotros + Equipo</a>
-              <a href="#">Contacto</a>
+              <BtnMen title={'Nuestro método'} id={'#'}/>
+              <BtnMen title={'Programas'} id={'#programs'}/>
+              <BtnMen title={'Sobre Nosotros + Equipo'} id={'#'}/>
+              <BtnMen title={'Contacto'} id={'#'}/>
             </div>
           </nav>
           <button className={`block md:hidden py-auto px-7  rounded focus:outline-none md:hover:bg-brown-100 focus:bg-brown-100  transition-all duration-300 ${scrollDirection === 'down' ? 'top-0': 'top-0'}`} onClick={()=>{setShowMenu(!showMenu)}}>
@@ -205,12 +248,12 @@ function Header(){
             <div className=' w-10 h-1 bg-brown-200 mb-2 '></div>
             <div className=' w-10 h-1 bg-brown-200 mb-2 '></div>
             <div className={`absolute top-0 right-0 w-full h-screen transition-all duration-500 bg-brown-200 ${showMenu ? 'opacity-100' : 'opacity-0 translate-y-[-60rem]'}`}>
-              <div className='fixed flex flex-col justify-center items-center h-screen w-full text-4xl gap-36'>
+              <div className='fixed flex flex-col justify-center items-center h-screen w-full text-3xl sm:text-4xl gap-24 sm:gap-36'>
                 <a href="#" className='px-10 hover:border-b-2'>Nuestro método</a>
                 <a href="#programs" className='px-10 hover:border-b-2'>Programas</a>
                 <a href="#" className='px-10 hover:border-b-2'>Sobre Nosotros + Equipo</a>
                 <a href="#" className='px-10 hover:border-b-2'>Contacto</a>
-                <a href="#" className='px-5 hover:border-b-2 text-2xl'>Cerrar</a>
+                <a href="#" className='px-5 hover:border-b-2 text-xl'>Cerrar</a>
               </div>
             </div>
           </button>
